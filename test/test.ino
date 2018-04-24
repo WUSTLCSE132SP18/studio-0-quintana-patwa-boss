b8644806ad665496336b8710c741ddcc65ad2412
apatwa17 <ahpandroid5@gmail.com>
2018-04-24 13:40:44 -0500

    commit

--- test/test.ino
+++ test/test.ino
@@ -1,13 +1,37 @@
-bool a = true;
-bool b = true;
-void setup(){
+enum State {
+  red,
+  purple,
+  green,
+  blue,
+  yellow,
+}
+State color = red;
+bool input = true;
+
+void setup() {
   Serial.begin(9600);
 }
 
-void loop(){
-  
-  int c = a == b;
-  Serial.println(c);
-  delay(2000);
+void loop() {
+  color = nextState(color);
+}
+
+State nextState(State current) {
+  switch(current){
+    case red:
+    if(input){
+      current = green;
+    } else {
+      current = purple;
+    }
+    break;
+    case purple:
+    if(input) {
+      current = blue;
+    } else {
+      current = purple;
+    }
+    case blue
+  }
 }
 
